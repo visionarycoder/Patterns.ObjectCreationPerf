828b6f507876ebb44e7101439d7d3239745c9f85
Ivan Jones <JonesIv@WSDOT.WA.GOV>
2024-11-19 19:10:11 -0800

    Downgrading to .Net 8

--- src/ObjectCreation.Client/Program.cs
+++ src/ObjectCreation.Client/Program.cs
@@ -1,9 +1,8 @@
-﻿using System.Diagnostics;
-
-using ObjectCreation.Client;
+﻿using ObjectCreation.Client;
 using Shared.Objects.Models;
+using System.Diagnostics;
 
-var ranges = new[] { 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
+var ranges = new[] { 1_000, 10_000, 100_000, 1_000_000 }; //, 10_000_000, 100_000_000, 1_000_000_000 };
 var results = new List<ExecutionSummary>();
 
 results.AddRange(Run<Object0>(ranges));
@@ -11,21 +10,18 @@ results.AddRange(Run<Object2>(ranges));
 results.AddRange(Run<Object4>(ranges));
 results.AddRange(Run<Object8>(ranges));
 
-Console.WriteLine("{0,15} | {1,15} | {2,15}", "Range", "Activator", "New");
-Console.WriteLine(new string('-', 50));
-Type? lastType = default;
+var typeName = "Type".PadLeft(20);
+var range = "Range".PadLeft(20);
+var activator = "Activator".PadLeft(20);
+var newColumn = "New".PadLeft(20);
+var header = new string($" {typeName} | {range} | {activator} | {newColumn}");
+
+Console.WriteLine(header);
+Console.WriteLine("".PadRight(header.Length, '-'));
 
 foreach (var result in results)
 {
-    
-    if (lastType != result.Type)
-    {
-        Console.WriteLine(result.Type.Name);
-        Console.WriteLine(new string('-', 50));
-        lastType = result.Type;
-    }
-    Console.WriteLine("{0,15:#,0} | {1,15} | {2,15}", result.Range, result.ActivatorTime, result.NewTime);
-
+    Console.WriteLine("{0,20} | {1,20:#,0} | {2,20} | {3,20}", result.Type.Name, result.Range, result.ActivatorTime, result.NewTime);
 }
 
 Console.WriteLine(new string('-', 50));
@@ -42,6 +38,8 @@ static List<ExecutionSummary> Run<T>(int[] ranges) where T : new()
 
     foreach (var range in ranges)
     {
+
+        Console.WriteLine($"Running {type.Name} for {range:#,0} iterations");
         stopwatch.Restart();
         foreach (var _ in Enumerable.Range(0, range))
         {
@@ -57,10 +55,9 @@ static List<ExecutionSummary> Run<T>(int[] ranges) where T : new()
         var newTime = stopwatch.Elapsed;
 
         list.Add(new ExecutionSummary(type, range, activatorTime, newTime));
+
     }
-    
+
     return list;
 
 }
-
-
